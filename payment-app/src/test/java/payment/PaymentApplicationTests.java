acb036def0f6c162e85c9e01714b4ff808fad241
pascalleclercq <pascal.leclercq@gmail.com>
2017-11-09 17:09:23 +0100

    refactored

--- payment-app/src/test/java/payment/PaymentApplicationTests.java
+++ payment-app/src/test/java/payment/PaymentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.atomikos.demotransactionalremoting;
+package payment;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoTransactionalRemotingApplicationTests {
+public class PaymentApplicationTests {
 
 	@Test
 	public void contextLoads() {
